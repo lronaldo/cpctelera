744f5d9587fb9d0bba24485784fa8486376c6e42
ronaldo <ronaldo@cheesetea.com>
2014-07-05 12:01:04 +0200

    * Added/Improved common legal comments to all the source files

--- cpctelera/src/sprites.s
+++ cpctelera/src/sprites.s
@@ -1,23 +1,20 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; This file is part of Cpctelera game engine for Amstrad CPC.           ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Cpctelera is free software: you can redistribute it and/or modify     ;;;
-;;; it under the terms of the GNU General Public License as published by  ;;;
-;;; the Free Software Foundation, either version 3 of the License, or     ;;;
-;;; (at your option) any later version.                                   ;;;
-;;;                                                                       ;;;
-;;; Cpctelera is distributed in the hope that it will be useful,          ;;;
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of        ;;;
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         ;;;
-;;; GNU General Public License for more details.                          ;;;
-;;;                                                                       ;;;
-;;; You should have received a copy of the GNU General Public License     ;;;
-;;; along with Cpctelera.  If not, see <http://www.gnu.org/licenses/>.    ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Author:  ronaldo / @FranGallegoBR                                     ;;;
-;;; (C) 2014 Fremos / Cheesetea / ByteRealms                              ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;
+;;-----------------------------LICENSE NOTICE------------------------------------
+;;  This file is part of CPCtelera: An Amstrad CPC Game Engine 
+;;  Copyright (C) 2014 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+;;
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+;;
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;-------------------------------------------------------------------------------
 ;#####################################################################
 ;### MODULE: Sprites                                               ###
 ;#####################################################################
