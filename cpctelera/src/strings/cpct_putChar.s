4ea71f5ad760b215727df723286ebb0e334959fb
ronaldo <ronaldo@cheesetea.com>
2015-04-23 14:28:02 +0200

    * Substituting SDCC's z80 putchar by new cpct_putchar ** cpct_putchar enables std-C printf use, using CPC firmware putchar routine

---
+++ cpctelera/src/strings/cpct_putChar.s
@@ -0,0 +1,70 @@
+;;-----------------------------LICENSE NOTICE------------------------------------
+;;  This file is part of CPCtelera: An Amstrad CPC Game Engine 
+;;  Copyright (C) 2015 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+;;
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU General Public License for more details.
+;;
+;;  You should have received a copy of the GNU General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;-------------------------------------------------------------------------------
+.module cpct_strings
+
+;;
+;; Include constants and general values
+;;
+.include /strings.s/
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; Function: void putchar(char c)
+;;
+;; Brief:
+;;    Prints a character on the screen using firmware routines. This function is 
+;; required by printf when we use C standard library.
+;;
+;; Details:
+;;    This function is required by C standard library to be able to use printf.
+;; It defines a way to print a given character to the screen. This version of the
+;; function makes use of firmware to print.
+;;
+;; Known limitations:
+;;    Firmware must be activated to be able to use this function.
+;; 
+;; Destroyed Register values: 
+;;    AF, BC, DE, HL
+;;
+;; Required memory:
+;;    14 bytes
+;;
+;; Time Measures:
+;; (start code)
+;; Case   | Cycles   | microSecs (us)
+;; ----------------------------------
+;; C Call | 49 + PC  | 12,25 + PCus
+;; Any    | 25 + PC  |  6,25 + PCus
+;; (end code)
+;;    PC = Time used in the call to firmware Print Charater function
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+_putchar::
+_putchar_rr_s:: 
+   ld   hl, #2    ;; [10] HL points to SP+2 (place where parameters start) 
+   add  hl, sp    ;; [11]
+   ld    a, (hl)  ;; [ 7] A = Character to be printed out
+
+   call  0xBB5A   ;; [11] Firmware function for printing characters
+   ret            ;; [10]
+
+_putchar_rr_dbs::
+   ld    a,e      ;; [ 4] A = Character to be printed out
+
+   call  0xBB5A   ;; [11] Firmware function for printing characters
+   ret            ;; [10]
--- cpctelera/tools/sdcc-3.4.3/src/device/lib/z80/putchar.s
+++ cpctelera/tools/sdcc-3.4.3/src/device/lib/z80/putchar.s
@@ -26,22 +26,23 @@
 ;   might be covered by the GNU General Public License.
 ;--------------------------------------------------------------------------
 
-        .area   _CODE
-_putchar::
-_putchar_rr_s::
-        ld      hl,#2
-        add     hl,sp
-
-        ld      l,(hl)
-        ld      a,#1
-        rst     0x08
-
-        ret
-
-_putchar_rr_dbs::
-        ld      l,e
-        ld      a,#1
-        rst     0x08
-
-        ret
+;;; DISABLED: Use CPCtelera's putchar instead
+;        .area   _CODE
+;_putchar::
+;_putchar_rr_s::
+;        ld      hl,#2
+;        add     hl,sp
+;
+;        ld      l,(hl)
+;        ld      a,#1
+;        rst     0x08
+;
+;        ret
+;
+;_putchar_rr_dbs::
+;        ld      l,e
+;        ld      a,#1
+;        rst     0x08
+;
+;        ret
 
