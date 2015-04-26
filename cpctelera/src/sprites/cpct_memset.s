7d3a2154f07553f609c0d19c6da85a384090ce89
ronaldo <ronaldo@cheesetea.com>
2015-04-27 00:17:21 +0200

    Improved performance of cpct_memset * 12 cycles less (10% faster) on constant stuff, at the cost of 3 more bytes of memory

--- cpctelera/src/sprites/cpct_memset.s
+++ cpctelera/src/sprites/cpct_memset.s
@@ -37,53 +37,40 @@
 ;### INPUTS (1 Byte)                                                  ###
 ;###  * (2B DE) Starting point in memory                              ###
 ;###  * (2B BC) Number of bytes to be set (should be greater than 1!) ###
-;###  * (1B A) Value to be set                                        ###
+;###  * (1B A ) Value to be set                                       ###
 ;########################################################################
 ;### EXIT STATUS                                                      ###
 ;###  Destroyed Register values: AF, BC, DE, HL                       ###
 ;########################################################################
-;### MEASURES (Way 2 for parameter retrieval from stack)              ###
-;### MEMORY:  17 bytes                                                ###
-;### TIME:    120 + 21*BC cycles (28.25 + 5.25*BC us)                 ###
+;### MEASURES                                                         ###
+;### MEMORY:  20 bytes                                                ###
+;### TIME:    108 + 21*NB cycles (27.00 + 5.25*BC us)                 ###
 ;########################################################################
 ;
 
 .globl _cpct_memset
 _cpct_memset::
+   ;; Recover parameters from stack
+   ld   hl, #2       ;; [10] Make HL point to the byte where parameters start in the
+   add  hl, sp       ;; [11] ... stack (first 2 bytes are return address)
+   ld    e, (hl)     ;; [ 7] DE = Pointer to first byte in memory for memset
+   inc  hl           ;; [ 6]
+   ld    d, (hl)     ;; [ 7] 
+   inc  hl           ;; [ 6]
+   ld    c, (hl)     ;; [ 7] BC = Amount of bytes in memory to set to the value of A
+   inc  hl           ;; [ 6]
+   ld    b, (hl)     ;; [ 7]
+   inc  hl           ;; [ 6] HL finally points to the value to be set
 
-.if let_disable_interrupts_for_function_parameters
-   ;; Way 1: Pop + Restoring SP. Faster, but consumes 4 bytes more, and requires disabling interrupts
-   LD  HL, #0        ;; [10] HL = SP (To quickly restore it later)
-   ADD HL, SP        ;; [11]
-   DI                ;; [ 4] Disable interrupts to ensure no one overwrites return address in the stack
-   POP AF            ;; [10] Get return value from stack
-   POP DE            ;; [10] (1st param) DE = Starting point in memory
-   POP BC            ;; [10] (2nd param) BC = Number of bytes to be set
-   DEC SP            ;; [ 6] ** 3rd param is byte-size, so need to decrease SP to load it into A when doing POP (otherwise it would go to F)
-   POP AF            ;; [10] (3rd param) A = Value to be set
-ms_restoreSP:
-   LD SP, HL         ;; [ 6] -- Restore Stack Pointer --
-   EI                ;; [ 4] Enable interrupts again
-.else 
-   ;; Way 2: Pop + Push. Just 6 cycles more, but does not require disabling interrupts
-   pop  af           ;; [10] Get return value from stack
-   pop  de           ;; [10] (1st param) DE = Starting point in memory
-   pop  bc           ;; [10] (2nd param) BC = Number of bytes to be set
-   pop  hl           ;; [10] (3rd param) L = Value to be set (H=random)
-   push hl           ;; [11] Restore Stack status pushing values again
-   push bc           ;; [11] (Interrupt safe way, 7 cycles more)
-   push de           ;; [11]
-   push af           ;; [11]
-   ld   a, l         ;; [ 4] A = L (Value to be set)
-.endif
+   ldi               ;; [16] (HL)->(DE) Copy value to the first byte of the memory to be set
 
    ;; Set up HL and DE for a massive copy of the Value to be set
-   LD H, D           ;; [ 4] HL = DE (Starting address in memory)
-   LD L, E           ;; [ 4] 
-   INC DE            ;; [ 6] DE++ (Next position in memory)
-   LD (HL), A        ;; [ 7] (HL) = A --> First item of the memory array = Value
-   DEC BC            ;; [ 6] BC--, We already have copied the first byte with previous instruction
+   ld    h, d        ;; [ 4] HL = DE (2nd byte of the memory array to be filled up)
+   ld    l, e        ;; [ 4] 
+   dec   hl          ;; [ 6] HL-- (Point to the first position of the memory array to be
+                     ;; .... filled up, which already contains the value to be set)
 
-   LDIR              ;; [21/16] Copy the reset of the bytes, cloning previous one
+   ;; Copy the rest of the bytes
+   ldir              ;; [21/16] Copy the reset of the bytes, cloning the first one
 
-   RET               ;; [10] Return 
+   ret               ;; [10] Return  
