552752f7d4c4b9de42ebd9b6c52de92d7d191988
ronaldo <ronaldo@cheesetea.com>
2015-04-29 14:50:41 +0200

    Removed stdio.h from measureCycles example

--- examples/medium/measureCycles/src/main.c
+++ examples/medium/measureCycles/src/main.c
@@ -17,7 +17,6 @@
 //  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //------------------------------------------------------------------------------
 
-#include <stdio.h>
 #include <cpctelera.h>
 #include "sprites.h"
 
