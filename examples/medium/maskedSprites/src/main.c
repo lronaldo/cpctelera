10682626748a36d4dc63b4d61dc293ea9ba462f2
ronaldo <ronaldo@cheesetea.com>
2015-05-09 19:44:57 +0200

    Minor details update * And Dardalorth added to credits in one file of maskedSprites

--- examples/medium/maskedSprites/src/main.c
+++ examples/medium/maskedSprites/src/main.c
@@ -1,6 +1,7 @@
 //-----------------------------LICENSE NOTICE------------------------------------
 //  This file is part of CPCtelera: An Amstrad CPC Game Engine
-//  Copyright (C) 2014-2015 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+//  Copyright (C) 2015 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+//  Copyright (C) 2015 Dardalorth / Fremos / Carlio
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
