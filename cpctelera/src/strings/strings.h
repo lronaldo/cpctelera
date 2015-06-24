d5caa9e7226643737e378e8e1a229714f37e1c78
ronaldo <ronaldo@cheesetea.com>
2015-06-24 18:31:56 +0200

    Improvement: using u8 for asciis on draw functions

--- cpctelera/src/strings/strings.h
+++ cpctelera/src/strings/strings.h
@@ -22,10 +22,10 @@
 #include <types.h>
 
 // Functions for drawing ROM Characters on Graphics Screen
-extern void cpct_drawCharM0     (void* video_memory, u8 fg_pen, u8 bg_pen, i8 ascii);
-extern void cpct_drawCharM1     (void* video_memory, u8 fg_pen, u8 bg_pen, i8 ascii);
-extern void cpct_drawCharM1_f   (void* video_memory, u8 fg_pen, u8 bg_pen, i8 ascii);
-extern void cpct_drawCharM2     (void* video_memory, u8 pen, i8 ascii);
+extern void cpct_drawCharM0     (void* video_memory, u8 fg_pen, u8 bg_pen, u8 ascii);
+extern void cpct_drawCharM1     (void* video_memory, u8 fg_pen, u8 bg_pen, u8 ascii);
+extern void cpct_drawCharM1_f   (void* video_memory, u8 fg_pen, u8 bg_pen, u8 ascii);
+extern void cpct_drawCharM2     (void* video_memory, u8 pen, u8 ascii);
 
 // Functions for drawing Strings with ROM Characters on Graphics Sceen
 extern void cpct_drawStringM0   (void* string, void* video_memory, u8 fg_pen, u8 bg_pen);
