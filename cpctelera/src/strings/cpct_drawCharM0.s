62178d4643af813755a550c1f4a11974eebbfdfa
ronaldo <ronaldo@cheesetea.com>
2015-06-24 20:51:53 +0200

    Modified API docs for string drawing functions * i8 ascii shown now as u8

--- cpctelera/src/strings/cpct_drawCharM0.s
+++ cpctelera/src/strings/cpct_drawCharM0.s
@@ -30,7 +30,7 @@
 ;; screen in Mode 0 (160x200 px, 16 colours).
 ;;
 ;; C Definition:
-;;    void <cpct_drawCharM0> (void* *video_memory*, <u8> *fg_pen*, <u8> *bg_pen*, <i8> *ascii*)
+;;    void <cpct_drawCharM0> (void* *video_memory*, <u8> *fg_pen*, <u8> *bg_pen*, <u8> *ascii*)
 ;;
 ;; Input Parameters (5 Bytes):
 ;;  (2B DE) video_memory - Video memory location where the character will be drawn
