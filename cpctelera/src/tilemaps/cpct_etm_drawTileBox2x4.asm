5934208502a1d2af844521c3422a585cf4a52561
ronaldo <ronaldo@cheesetea.com>
2015-08-02 14:53:00 +0200

    Reviewed cpct_etm_drawTileBox2x4 documentation * Also improved code by some cycles moving a block

--- cpctelera/src/tilemaps/cpct_etm_drawTileBox2x4.asm
+++ cpctelera/src/tilemaps/cpct_etm_drawTileBox2x4.asm
@@ -19,12 +19,14 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
-;; Function: cpct_etm_redrawTileBox
+;; Function: cpct_etm_drawTileBox2x4
 ;;
-;;    Redraws a determined box of tiles inside the tilemap.
+;;    Draws (or redraws) a determined rectangle of tiles inside a tilemap. It is 
+;; mainly used to restore parts of a tilemap.
 ;;
 ;; C Definition:
-;;    void <cpct_etm_redrawTileBox> (void* *ptilemap*, u8 *x*, u8 *y*, u8 *w*, u8 *h*) __z88dk_callee;
+;;    void <cpct_etm_drawTileBox2x4> (<u8> *x*, <u8> *y*, <u8> *w*, <u8> *h*, <u8> *map_width*,
+;;                                    void* *pvideomem*, const void* *ptilemap*, ) __z88dk_callee;
 ;;
 ;; Input Parameters (9 bytes):
 ;;    (1B  A) map_width - Width in tiles of a complete row of the tilemap
@@ -32,36 +34,56 @@
 ;;    (1B  B) y         - y tile-coordinate of the starting tile inside the tilemap
 ;;    (1B  L) w         - width in tiles of the tile-box to be redrawn
 ;;    (1B  H) h         - height in tiles of the tile-box to be redrawn
-;;    Stack:
+;;    * Always received on the stack
 ;;    (2B) pvideomem    - Pointer to the place on video memory (or backbuffer) where to draw the tilebox
 ;;    (2B) ptilemap     - Pointer to the start of the tilemap
 ;;
 ;; Assembly call (Input parameters on registers):
-;;    > call cpct_etm_redrawTileBox_asm
+;;    > call cpct_etm_drawTileBox2x4_asm
+;;    It is important to notice that part of the parameters are received on 
+;; registers and part of them on the stack. This would be an example code for
+;; calling this function,
+;; (start code)
+;;    ;; Set Parameters on the stack
+;;    ld   hl, #ptilemap   ;; HL = pointer to the tilemap
+;;    push hl              ;; Push ptilemap to the stack
+;;    ld   hl, #pvideomem  ;; HL = Pointer to video memory location where tilemap is drawn
+;;    push hl              ;; Push pvideomem to the stack
+;;    ;; Set Paramters on registers
+;;    ld    a, #map_width  ;; A = map_width
+;;    ld    b, #y          ;; B = x tile-coordinate
+;;    ld    c, #x          ;; C = y tile-coordinate
+;;    ld    h, #h          ;; H = height in tiles of the tile-box
+;;    ld    l, #w          ;; L =  width in tiles of the tile-box
+;;    call  cpct_etm_drawTileBox2x4_asm ;; Call the function
+;; (end code)
 ;;
 ;; Parameter Restrictions:
-;;    * *ptilemap* could be any 16-bits value, representing the memory 
-;; address where the <cpct_TEasyTilemap> structure is stored. This function expects
-;; the parameter to point to a complete structure like this: if the structure is not
-;; well populated or the pointer points to other thing, the result is undefined (most
-;; likely a crash or rubbish on the screen).
-;;    * (*x*, *y*) represent the coordinates, in tiles, of the upper-left tile of 
-;; the tilebox to be redrawn. These coordinates are not checked to be inside the 
-;; tilemap, and providing bad coordinates leads to undefined behaviour. On a rectangular
-;; screen, *x* would be in the range [0,39] and *y* in the range [0,49].
+;;    * (*x*, *y*) represent the coordinates, in tiles, of the upper-left corner of 
+;; the tilebox to be drawn. These coordinates are relative to the upper-left corner of
+;; the tilemap (its first tile). They are not checked to be inside the tilemap, and 
+;; providing bad coordinates leads to undefined behaviour. On a standard mode screen,
+;; *x* would be in the range [1,40] and *y* in the range [1,50].
 ;;    * (*w*, *h*) represent the width and height, in tiles, of the tilebox being
-;; redrawn. Same as (*x*,*y*), these values are not checked and values drawing outside
-;; the tilemap could be provided, and would lead to undefined behaviour. 
+;; drawn. Same as (*x*,*y*), these values are not checked and values drawing outside
+;; the tilemap or beyond the screen could be provided, and would lead to undefined behaviour. 
+;;    * *map_width* is expected to be the width of the tilemap in tiles. Do not confuse
+;; this value with *w*, that represents the width of the tilebox (please note the difference).
+;;    * *pvideomem* should be a pointer to the video memory or backbuffer address where
+;; the upper-left corner of the tilemap is drawn. Please note that it should point to the
+;; start of the tilemap, and *NOT* the start of the tilebox, in video memory or backbuffer.
+;;    * *ptilemap* should be a 2-bytes pointer to the start of a tilemap definition in
+;; Easytilemaps format: a 2D matrix of 1-byte tile-indexes. If it points to a different
+;; kind of data, the result is undefined (most likely a crash or rubbish on the screen).
 ;;
 ;; Known limitations:
 ;;     * This function does not do any kind of checking over the tilemap, its
 ;; contents or size. If you give a wrong pointer, your tilemap has different
 ;; dimensions than required or has less / more tiles than will be used later,
 ;; rubbish can appear on the screen.
-;;     * Inside the structure, the pointer to the video memory *must* point to a
-;; pixel line 0 from any character line on the screen or back buffer. To know where 
-;; pixel lines 0 are located you may have a look at <cpct_drawSprite> documentation. 
-;; This function *does not work if pvideomem points to a pixel line 4*.
+;;     * The *pvideomem* pointer *must* point to a *pixel line 0 or pixel line 4* from 
+;; any character line on the screen or back buffer. To know where pixel lines 0/4 are 
+;; located you may have a look at <cpct_drawSprite> documentation.
 ;;     * This function only draws 8-bytes tiles of size 2x4 (in bytes).
 ;;     * This function *will not work from ROM*, as it uses self-modifying code.
 ;;     * Under hardware scroll conditions, tile drawing will fail if asked to draw
@@ -69,11 +91,32 @@
 ;; next screen byte at that locations is -0x7FF and not +1 bytes away.
 ;;
 ;; Details:
-;;    This function draws tilebox (a group of tiles forming a rectangular box), a portion
-;; of the tilemap, to the screen or to a backbuffer. 
+;;    This function draws tile-box, a portion of a concrete tilemap, to the screen 
+;; or to a backbuffer. A tile-box is a group of tiles forming a rectangular box, and being
+;; part of a tilemap. Data is expected to be in Easytilemaps format, with the tilemap
+;; being a 2D matrix of 1-byte tile-indexes, and tileset being an array of pointers
+;; (2-bytes each) to tiles. Each tile is expected to be an array of 2x4-bytes in
+;; screen pixel format.
+;; 
+;;    The main intended use of this function is to erase entities that
+;; move over a tilemap, redrawing the portion of the tilemap (a tilebox) over the
+;; moving entity.
 ;;
-;;    Each tile from the tileset must be a 8-bytes, 2x4-sized tile. This function calls
-;; <cpct_drawTileAligned2x4_f> for drawing each one of the tiles. 
+;;    So, first 4 parameters (*x*, *y*, *w*, *h*) of these function define the tilebox as a portion of the
+;; given tilemap (last parameter). First 2 parameters locate the upper-left corner of
+;; the tilebox (*x*, *y*) and the next two define its width (*w*) and height (*h*) in tiles.
+;; *x* and *y* coordinates are considered relative to the upper-left corner of the 
+;; tilemap (which is the origin of coordinates, (0,0)), with *x*'s growing right and
+;; *y*'s growing down.
+;;
+;;    This function uses the internal pointer to the tileset (ptileset) for Easytilemaps.
+;; This internal pointer *MUST HAVE BEEN SET PREVIOUSLY* to calling this function. 
+;; <cpct_etm_setTileset2x4> may be used for setting this internal pointer. Once set
+;; it remains unless manually reset.
+;;
+;;    This function calls <cpct_etm_drawTileRow2x4>, which also calls <cpct_drawTileAligned2x4_f>.
+;; So, these two functions will also be included in your code when using <cpct_etm_drawTileBox2x4>
+;; (using their assembly bindings).
 ;;
 ;; Destroyed Register values: 
 ;;      AF,  BC,  DE,  HL
@@ -83,19 +126,31 @@
 ;;
 ;; Time Measures:
 ;; (start code)
-;;    Case     |      microSecs (us)            |          CPU Cycles              |
-;; ---------------------------------------------------------------------------------
-;;    Any      | 
-;; ---------------------------------------------------------------------------------
-;; ---------------------------------------------------------------------------------
+;;    Case     |      microSecs (us)                |              CPU Cycles                 |
+;; --------------------------------------------------------------------------------------------
+;;    Any      | 98 + PX + MY + 7PPY + (37 + 103W)H | 392 + 4PX + 4MY + 28PPY + (148 + 412W)H |
+;; --------------------------------------------------------------------------------------------
+;;  ASM-Saving |            - 11                    |               - 44                      |
+;; --------------------------------------------------------------------------------------------
+;;  W=2, H=3   |         [ 845 - 937 ]              |           [ 3380 - 3748 ]               |
+;; --------------------------------------------------------------------------------------------
+;;  W=4, H=6   |        [ 2824 - 2916 ]             |          [ 11296 - 11664 ]               |
+;; --------------------------------------------------------------------------------------------
 ;; (end code)
-;; W  = Map width (number of horizontal tiles)
-;; H  = Map height (number of vertical tiles)
-;; HO = Number of odd tile rows.
-;; HE = Number of even tile rows.
-;; For HO and HE, the first row is considered odd if it starts at a pixel line 4,
-;; and it is considered even otherwise. The last row of the tilemap is never taken 
-;; into account (either for HO or HE).
+;; W   : Tilebox width (number of horizontal tiles)
+;; H   : Tilebox height (number of vertical tiles)
+;; PX  : Extra cycles to add when *y* parameter is odd, for a intra-character lines jump
+;;       PX = { 0 - for even values of *y*, 
+;;            11 - for  odd values of *y* and tilemap starting at a Pixel Line 0,
+;;            13 - for  odd values of *y* and tilemap starting at a Pixel Line 4 }
+;; MY  : Cost in cycles of multiplying *map_width* times *y*. Depends on the value of *y*,
+;;       and can be calculated with this formula:
+;;           MY = 12*int(log2(y)) + 2*onbits(y) - 1, 
+;;       being onbits(y) = the number of bits that y has set to 1.
+;;       MY is in range [11 - 83]
+;; PPY : Number of total rows of the tilebox that start at a pixel line 4, not
+;;       counting the last row of the tilebox (if it is at a pixel line 4).
+;;       It corresponds to PPY = int(H/2) or PPY = int(H/2) - 1
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Declare tile drawing function we are going to use
@@ -244,6 +299,17 @@ tb_row_width:
 ;; Draw the tile-box
 ;;
 drawtiles_height:
+   ;; HL ends pointing to the byte the tilemap after the last drawn tile
+   ;; Make HL point to the start of the next tilebox row
+   ld    a, l      ;; [1] | HL += offset from end of a tilebox row to
+next_row_jump:     ;;     |      the start of the next one 
+   add   #00       ;; [2] |      (HL += 2 (map_width - tilebox_row_width))
+   ld    l, a      ;; [1] |
+   adc   h         ;; [1] |
+   sub   l         ;; [1] |
+   ld    h, a      ;; [1] |
+
+   ;; Calculate next location in video memory to draw 
    ld    a, d      ;; [1] DE += 0x2000 (0x800 x 4) to jump 4 pixel lines from here
    add  #0x20      ;; [2] 
    ld    d, a      ;; [1]
@@ -275,16 +341,7 @@ draw_next_row:
    exx             ;; [1] Back to normal register set
 
 ;; (B) numtiles (DE') pvideomem (HL) ptilemap
-   call  cpct_etm_drawTileRow2x4_asm
-
-   ;; Make HL point to the start of the next tilebox row
-   ld    a, l      ;; [1] | HL += offset from end of a tilebox row to
-next_row_jump:     ;;     |      the start of the next one 
-   add   #00       ;; [2] |      (HL += 2 (map_width - tilebox_row_width))
-   ld    l, a      ;; [1] |
-   adc   h         ;; [1] |
-   sub   l         ;; [1] |
-   ld    h, a      ;; [1] |
+   call  cpct_etm_drawTileRow2x4_asm ;; [7 + 103W]
 
    dec  c                   ;; [1] 1 less tile row to draw
    jr  nz, drawtiles_height ;; [2/3] If there still are some tile rows to draw (C!=0), continue
