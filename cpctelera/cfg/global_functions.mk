03260e4644289de0f4a462e2e1b30d7fa6bbf60e
ronaldo <ronaldo@cheesetea.com>
2016-03-12 00:31:47 +0100

    Renamed IMG2C to IMG2SPRITES * Reserved for sprites, as no tilesets are generated

--- cpctelera/cfg/global_functions.mk
+++ cpctelera/cfg/global_functions.mk
@@ -217,23 +217,23 @@ $(1): $(2)
 endef
 
 #################
-# IMG2C: General rule to convert images into C arrays representing
-# sprites, tiles, etc. Updates IMGCFILES and OBJS2CLEAN adding new C files
+# IMG2SPRITES: General rule to convert images into C arrays representing
+# sprites. Updates IMGCFILES and OBJS2CLEAN adding new C files
 # that result from image conversions
 #
-# $(1): Image file to be converted into C
+# $(1): Image file to be converted into C sprite
 # $(2): Graphics mode (0,1,2) for the generated values
 # $(3): Prefix to add to all C-identifiers generated
 # $(4): Width in pixels of each sprite/tile/etc that will be generated
 # $(5): Height in pixels of each sprite/tile/etc that will be generated
 # $(6): Firmware palette used to convert the image file into C values
 #
-define IMG2C
+define IMG2SPRITES
 IMGCFILES  := $(basename $(1)).c $(IMGCFILES)
 OBJS2CLEAN := $(basename $(1)).c $(basename $(1)).h $(OBJS2CLEAN)
-.INTERMEDIATE: $(basename $(1)).c $(basename $(1)).h
+.SECONDARY: $(basename $(1)).c $(basename $(1)).h
 $(basename $(1)).c $(basename $(1)).h: $(1)
 	@$(call PRINT,$(PROJNAME),"Generating C-arrays for images in $(1)...")
-	@cpct_img2tileset -m "$(2)" -bn "$(3)" -tw "$(4)" -th "$(5)" -pf $(6) $(1)
+	@cpct_img2tileset -nt -m "$(2)" -bn "$(3)" -tw "$(4)" -th "$(5)" -pf $(6) $(1)
 	@$(call PRINT,$(PROJNAME),"C-arrays generated for $(1)")
 endef
