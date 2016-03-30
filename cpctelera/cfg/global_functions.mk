5e970d21e8e796a8c44490573850e8bc0c3b15f8
lronaldo <ronaldo@cheesetea.com>
2016-03-30 17:26:53 +0200

    Improved version of IMG2SPRITES * Code is much clearer * Added 9th parameter for outputing hardware palette values

--- cpctelera/cfg/global_functions.mk
+++ cpctelera/cfg/global_functions.mk
@@ -230,6 +230,7 @@ endef
 # $(7): (mask,tileset,) "mask":    generate interlaced mask for all sprites converted
 #                       "tileset": generate a tileset array including pointers to all sprites
 # $(8): Output subfolder for generated .C and .H files (inside project folder)
+# $(9): (hwpalette) "hwpalette":   output palette array as hardware values
 #
 define IMG2SPRITES
 $(eval I2S_C  := $(basename $(1)).c)
@@ -239,16 +240,12 @@ $(eval I2S_NH := $(notdir $(I2S_H)))
 $(eval I2S_C2 := $(shell if [ ! "$(8)" = "" ]; then echo "$(8)/$(I2S_NC)"; else echo "$(I2S_C)"; fi))
 $(eval I2S_H2 := $(shell if [ ! "$(8)" = "" ]; then echo "$(8)/$(I2S_NH)"; else echo "$(I2S_H)"; fi))
 $(eval I2S_CH := $(I2S_C2) $(I2S_H2))
+$(eval I2S_P  := $(shell if [ "$(7)" = "mask" ]; then echo "-nt -im"; elif [ ! "$(7)" = "tileset" ]; then echo "-nt"; fi))
+$(eval I2S_P  := $(I2S_P) $(shell if [ "$(9)" = "hwpalette" ]; then echo "-oph"; fi))
 .SECONDARY: $(I2S_CH)
 $(I2S_CH): $(1)
 	@$(call PRINT,$(PROJNAME),"Converting $(1) into C-arrays...")
-	@if [ "$(7)" = "mask" ]; then \
-	   cpct_img2tileset -nt -m "$(2)" -bn "$(3)" -tw "$(4)" -th "$(5)" -pf $(6) -im $(1); \
-	elif [ "$(7)" = "tileset" ]; then \
-	   cpct_img2tileset     -m "$(2)" -bn "$(3)" -tw "$(4)" -th "$(5)" -pf $(6) $(1); \
-	else \
-	   cpct_img2tileset -nt -m "$(2)" -bn "$(3)" -tw "$(4)" -th "$(5)" -pf $(6) $(1); \
-	fi
+	cpct_img2tileset $(I2S_P) -m "$(2)" -bn "$(3)" -tw "$(4)" -th "$(5)" -pf $(6) $(1);
 	@$(call PRINT,$(PROJNAME),"Moving generated files:")
 	@$(call PRINT,$(PROJNAME)," - '$(I2S_C)' > '$(I2S_C2)'")
 	@$(call PRINT,$(PROJNAME)," - '$(I2S_H)' > '$(I2S_H2)'")
