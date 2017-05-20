01046978cec15ca3bb380ae972ed694cfadca439
ronaldo <ronaldo@cheesetea.com>
2017-05-20 10:23:09 +0200

    Fix: Prevent iDSK from removing same-name files     * iDSK was removing consecutive file entries with same name but different extension     * This fixes #48

--- cpctelera/tools/iDSK-0.13/src/GestDsk.cpp
+++ cpctelera/tools/iDSK-0.13/src/GestDsk.cpp
@@ -1072,12 +1072,13 @@ void DSK::RemoveFile ( int item ) {
 	strcpy( NomFic, GetNomAmsdos( TabDir[ i ].Nom ) );
 	char *p ;
 	
-	do {
+	//do {
 		TabDir[ i ].User = USER_DELETED;
 		SetInfoDirEntry( i, &TabDir[ i ]);
-		p = GetNomAmsdos( TabDir[ ++i ].Nom) ;
-	} while ( ! strncmp( NomFic, p , max(strlen( p ), strlen( NomFic ) )) );
-	
+	//	p = GetNomAmsdos( TabDir[ ++i ].Nom) ;
+	//} while ( ! strncmp( NomFic, p , max(strlen( p ), strlen( NomFic ) )) );
+	//
+   // << Prevent consecutive entries with same name but different extension from being deleted
 		
 	return ;
 }
