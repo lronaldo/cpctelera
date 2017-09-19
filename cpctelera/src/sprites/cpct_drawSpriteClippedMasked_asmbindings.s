c4c4f8f72ba36eaaece749eb09626d812b8879b0
Arnaud <arnaud_bouche@hotmail.com>
2017-09-19 21:08:29 +0200

    Add draw sprite masked clipped

---
+++ cpctelera/src/sprites/cpct_drawSpriteClippedMasked_asmbindings.s
@@ -0,0 +1,35 @@
+;;-----------------------------LICENSE NOTICE------------------------------------
+;;  This file is part of CPCtelera: An Amstrad CPC Game Engine 
+;;  Copyright (C) 2015 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+;;
+;;  This program is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Lesser General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  This program is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU Lesser General Public License for more details.
+;;
+;;  You should have received a copy of the GNU Lesser General Public License
+;;  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;-------------------------------------------------------------------------------
+.module cpct_sprites
+
+;; Macros for easy use of undocumented opcodes
+.include "../macros/cpct_undocumentedOpcodes.s"
+
+;;
+;; ASM bindings for <cpct_drawSpriteClippedMasked>
+;;
+cpct_drawSpriteClippedMasked_asm::   ;; Assembly entry point
+
+   pop  hl                     ;; [3] HL = Return address
+   ld (simulated_return+1), hl ;; [5] Save return address for simulated return
+
+.include /cpct_drawSpriteClippedMasked.asm/
+
+simulated_return:
+   ld   hl, #0000              ;; [3] HL = return address
+   jp   (hl)                   ;; [1] Do a manual "ret"
