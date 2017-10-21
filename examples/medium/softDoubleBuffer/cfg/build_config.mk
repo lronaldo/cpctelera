31480634de421bb716e1d3bff41c754357843761
ronaldo <ronaldo@cheesetea.com>
2017-10-22 00:34:18 +0200

    Renamed and reclassified example     - advanced/backBuffer -> medium/softDoubleBuffer

--- examples/medium/softDoubleBuffer/cfg/build_config.mk
+++ examples/medium/softDoubleBuffer/cfg/build_config.mk
@@ -49,7 +49,7 @@
 
 # Name of the project (without spaces, as it will be used as filename)
 #   and Z80 memory location where code will start in the generated binary
-PROJNAME   := draw2spr
+PROJNAME   := softbuff
 Z80CODELOC := 0x4000
 
 ##
