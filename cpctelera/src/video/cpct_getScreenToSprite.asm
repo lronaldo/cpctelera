4c60a7501bb21a9adac75d0a67584e44950baa0a
Arnaud <arnaud_bouche@hotmail.com>
2018-01-20 14:14:23 +0100

    Update cpct_getScreenToSprite.asm

--- cpctelera/src/video/cpct_getScreenToSprite.asm
+++ cpctelera/src/video/cpct_getScreenToSprite.asm
@@ -1,7 +1,7 @@
 ;;-----------------------------LICENSE NOTICE------------------------------------
 ;;  This file is part of CPCtelera: An Amstrad CPC Game Engine 
-//  Copyright (C) 2018 Arnaud BOUCHE
-//  Copyright (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+;;  Copyright (C) 2018 Arnaud BOUCHE
+;;  Copyright (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
 ;;
 ;;  This program is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU Lesser General Public License as published by
