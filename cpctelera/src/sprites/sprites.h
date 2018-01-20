c71041e8ac8c3644216dd3f8e2c515cad8c005de
ronaldo <ronaldo@cheesetea.com>
2018-01-20 18:14:54 +0100

    Moved getScreenToSprite to sprites/screenToSprite

--- cpctelera/src/sprites/sprites.h
+++ cpctelera/src/sprites/sprites.h
@@ -1,6 +1,6 @@
 //-----------------------------LICENSE NOTICE------------------------------------
 //  This file is part of CPCtelera: An Amstrad CPC Game Engine
-//  Copyright (C) 2014-2017 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+//  Copyright (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
@@ -31,6 +31,7 @@
 #include "sprite_types.h"
 #include "transparency_table_macros.h"
 #include "drawToSpriteBuffer/drawToSpriteBuffer.h"
+#include "screenToSprite/screenToSprite.h"
 #include "drawTile/drawTile.h"
 #include "flipping/flipping.h"
 #include "blending/blending.h"
