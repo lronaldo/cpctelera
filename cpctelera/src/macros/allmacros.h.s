2821498ad18996e385a3f052b28e940eef917449
ronaldo <ronaldo@cheesetea.com>
2018-01-27 01:26:20 +0100

    Modified allmacros.h.s to properly include luts

--- cpctelera/src/macros/allmacros.h.s
+++ cpctelera/src/macros/allmacros.h.s
@@ -20,7 +20,7 @@
 .include "macros/cpct_opcodeConstants.h.s"
 .include "macros/cpct_reverseBits.h.s"
 .include "macros/cpct_undocumentedOpcodes.h.s"
-.include "macros/cpct_tableConversions.h.s"
+.include "macros/cpct_luts.h.s"
 
 ;;//////////////////////////////////////////////////////////////////////
 ;; Group: General Useful Macros
