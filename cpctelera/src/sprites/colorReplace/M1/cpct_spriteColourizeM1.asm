a5feaa5d1c4c448c62dd24fdba07c8502ab7ff72
ronaldo <ronaldo@cheesetea.com>
2018-02-02 00:01:44 +0100

    Improved performance for spriteColourizeM1     * 1 less microsecond in the inner loop, and 2 less outside.     * Also added measures and reviewed commentaries

--- cpctelera/src/sprites/colorReplace/M1/cpct_spriteColourizeM1.asm
+++ cpctelera/src/sprites/colorReplace/M1/cpct_spriteColourizeM1.asm
@@ -58,18 +58,18 @@
 ;;    AF, BC, DE, HL
 ;;
 ;; Required memory:
-;;     C-bindings - 38 bytes
-;;   ASM-bindings - 35 bytes
+;;     C-bindings - 60 bytes
+;;   ASM-bindings - 57 bytes
 ;;
 ;; Time Measures:
 ;; (start code)
 ;;  Case      |   microSecs (us)       |        CPU Cycles
 ;; ----------------------------------------------------------------
-;; Best  Case |   21 + (5 + 27W)H      |   84 + (20 + 108W)H
-;; Worst Case |   21 + (5 + 29W)H      |   84 + (20 + 116W)H
+;; Best  Case |   19 + (5 + 48W)H      |   76 + (20 + 192W)H
+;; Worst Case |   19 + (5 + 51W)H      |   76 + (20 + 204W)H
 ;; ----------------------------------------------------------------
-;;  W=2,H=16  |      965 / 1029        |       3860 /  4116
-;;  W=4,H=32  |     3637 / 3893        |      14548 / 15572
+;;  W=2,H=16  |     1635 / 1731        |       6540 /  6924
+;;  W=4,H=32  |     6323 / 6707        |      25292 / 26828
 ;; ----------------------------------------------------------------
 ;; Asm saving |         -12            |          -48
 ;; ----------------------------------------------------------------
@@ -84,77 +84,77 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Save width value to restore it after each line 
-ld     a, b          ;; [1] A = width
-ld    (w_restore), a ;; [4] Save width into its restore place
-
-;; As E register is free, use it as a cache for Pixel 3 Pattern of the colour to be replaced
-px3_oldval = .+1
-ld     e, #00        ;; [2] E = Pixel 3 2-bit Pattern of the colour to be replaced (0xxx1xxx)
+ld     a, b              ;; [1] A = width
+ld    (w_restore), a     ;; [4] Save width into its restore place
 
 ;; Loop through all the bytes of the sprite, replacing colours that have the same
 ;; 2-bit pattern of the colour we want to replace. 
 loop:
-   ;; Check and replace Pixel 3
-   ld     a, (hl)    ;; [2] A = Byte with 4 Mode 1 Pixels to be replaced
-   and   #0b10001000 ;; [2] A ^= 0x88. Left out only the 2 bits of the Pixel 0 (0xxx1xxx)
-   ld     d, a       ;; [1] D = holds a copy of Pixel 3 bits, just in case we don't have to replace it
-   cp     e          ;; [1] A == E? Check if A is equal to the Pixel 3 Pattern of the colour we want to replace
-   jr    nz, notpx3  ;; [2/3] If it is not equal, just continue to check pixel 0
+   ;; Check and replace Pixel-3
+   ld     e, (hl)        ;; [2] E = Byte with 4 Mode-1 Pixels to be replaced
+   ld     a, #0b10001000 ;; [2] A = 0x88. Mask for the 2 bits of Pixel-3
+   and    e              ;; [1] A = E ^ A. Left out only the 2 bits of Pixel-3 (1xxx0xxx)
+   ld     d, a           ;; [1] D = holds a copy of Pixel-3 bits, just in case we don't have to replace it
+   px3_oldval = .+1
+   cp     #00            ;; [2] Check if A is equal to the Pixel-3 Pattern of the colour we want to replace
+   jr    nz, notpx3      ;; [2/3] If it is not equal, just continue to check Pixel-2
       px3_newval = .+1
-      ld     d, #00  ;; [2] Perform replacement of Pixel 3. D holds the 2 bits of the new colour. #00 is a placeholder
+      ld     d, #00      ;; [2] Perform replacement of Pixel-3. D holds the 2 bits of the new colour. #00 is a placeholder
    notpx3:
 
-   ;; Check and replace Pixel 2
-   ld     a, (hl)    ;; [2] A = Restore the value of the byte with the 4 Mode 1 pixels to be replaced
-   and   #0b01000100 ;; [2] A ^= 0x44. Left out only the 2 bits of the Pixel 0 (0xxx1xxx)
+   ;; Check and replace Pixel-2
+   ld     a, #0b01000100 ;; [2] A = 0x44. Mask for the 2 bits of Pixel-2
+   and    e              ;; [1] A = E ^ A. Left out only the 2 bits of Pixel-2 (x1xxx0xx)
    px2_oldval = .+1
-   cp    #00         ;; [2] Check if A is equal to the Pixel 0 Pattern of the colour we want to replace. #00 is a placeholder
-   jr    nz, notpx2  ;; [2/3] If it is not equal, just continue to mix both output values
+   cp    #00             ;; [2] Check if A is equal to the Pixel-2 Pattern of the colour we want to replace. #00 is a placeholder
+   jr    nz, notpx2      ;; [2/3] If it is not equal, just continue to mix both output values
       px2_newval = .+1
-      ld     a, #00  ;; [2] Perform replacement of Pixel 0. A holds the 4 bits of the new colour. #00 is a placeholder
+      ld     a, #00      ;; [2] Perform replacement of Pixel-2. A holds the 2 bits of the new colour. #00 is a placeholder
    notpx2:
 	  
-   ;; Mix both replacements
-   or     d          ;; [1] A |= D. A and D hold replacements for pixels 0 and 1. This mixes them into A.
-   ld  d, a          ;; [1] D = A. Save current replacements
+   ;; Mix Pixel-3 and Pixel-2 replacements
+   or     d              ;; [1] A |= D. A and D hold replacements for pixels 3 and 2. This mixes them into A.
+   ld     d, a           ;; [1] D = A. Save current replacements into D
    
-   ;; Check and replace Pixel 1
-   ld     a, (hl)    ;; [2] A = Restore the value of the byte with the 4 Mode 1 pixels to be replaced
-   and   #0b00100010 ;; [2] A ^= 0x22. Left out only the 2 bits of the Pixel 0 (0xxx1xxx)
+   ;; Check and replace Pixel-1
+   ld     a, #0b00100010 ;; [2] A = 0x22. Mask for the 2 bits of Pixel-1
+   and    e              ;; [1] A = E ^ A. Left out only the 2 bits of Pixel-1 (xx1xxx0x)
    px1_oldval = .+1
-   cp    #00         ;; [2] Check if A is equal to the Pixel 0 Pattern of the colour we want to replace. #00 is a placeholder
-   jr    nz, notpx1  ;; [2/3] If it is not equal, just continue to mix both output values
+   cp    #00             ;; [2] Check if A is equal to the Pixel-1 Pattern of the colour we want to replace. #00 is a placeholder
+   jr    nz, notpx1      ;; [2/3] If it is not equal, just continue to mix both output values
       px1_newval = .+1
-      ld     a, #00  ;; [2] Perform replacement of Pixel 0. A holds the 4 bits of the new colour. #00 is a placeholder
+      ld     a, #00      ;; [2] Perform replacement of Pixel-1. A holds the 2 bits of the new colour. #00 is a placeholder
    notpx1:
    
-   ;; Mix with previous replacements
-   or     d          ;; [1] A |= D. A and D hold replacements for pixels 0, 1 and 2. This mixes them into A.
-   ld  d, a          ;; [1] D = A. Save current replacements
+   ;; Mix Pixel-1 with previous Pixel-3 & Pixel-2 already mixed replacements
+   or     d              ;; [1] A |= D. D hold mixed replacements for pixels 3 and 2.
+                         ;; ....A holds replacement for pixel-1. Both 3 replacements get mixed into A.
+   ld     d, a           ;; [1] D = A. Save current replacements into D
    
-    ;; Check and replace Pixel 0
-   ld     a, (hl)    ;; [2] A = Restore the value of the byte with the 4 Mode 1 pixels to be replaced
-   and   #0b00010001 ;; [2] A ^= 0x11. Left out only the 2 bits of the Pixel 0 (0xxx1xxx)
+    ;; Check and replace Pixel-0
+   ld     a, #0b00010001 ;; [2] A = 0x11. Mask for the 2 bits of Pixel-0
+   and    e              ;; [1] A = E ^ A. Left out only the 2 bits of Pixel-0 (xxx1xxx0)
    px0_oldval = .+1
-   cp    #00         ;; [2] Check if A is equal to the Pixel 0 Pattern of the colour we want to replace. #00 is a placeholder
-   jr    nz, notpx0  ;; [2/3] If it is not equal, just continue to mix both output values
+   cp    #00             ;; [2] Check if A is equal to the Pixel-0 Pattern of the colour we want to replace. #00 is a placeholder
+   jr    nz, notpx0      ;; [2/3] If it is not equal, just continue to mix both output values
       px0_newval = .+1
-      ld     a, #00  ;; [2] Perform replacement of Pixel 0. A holds the 4 bits of the new colour. #00 is a placeholder
+      ld     a, #00      ;; [2] Perform replacement of Pixel-0. A holds the 2 bits of the new colour. #00 is a placeholder
    notpx0:
    
-   ;; Mix  with previous replacements and save
-   or     d          ;; [1] A |= D. A and D hold replacements for pixels 0, 1, 2, 3 and 4. This mixes them into A.
-   ld   (hl), a      ;; [2] Write byte with colours replaced
-   inc   hl          ;; [2] HL++ Move to next byte of the sprite
+   ;; Mix Pixel-0 with previous replacements and save
+   or     d          ;; [1] A |= D. D hold mixed replacements for pixels 3, 2 and 1.
+                     ;; ....A holds replacement for Pixel-0. Both 4 replacements get mixed into A.
+   ld   (hl), a      ;; [2] Write byte with colours all 4 colours replaced
+   inc   hl          ;; [2] HL++. Move to next byte of the sprite
 
- djnz  loop          ;; [3/4] B--. Continue looping if there are more bytes left in this sprite line (B!=0)
+djnz  loop           ;; [3/4] B--. Continue looping if there are more bytes left in this sprite line (B!=0)
 
    w_restore=.+1
    ld    b, #00      ;; [2] B = width (restore value). #00 is a placeholder
    dec   c           ;; [1] C-- (One less line of the sprite to process)
- jr    nz, loop      ;; [2/3] Continue looping if there are more lines left
+jr    nz, loop       ;; [2/3] Continue looping if there are more lines left
 
- ret                 ;; [3] Return to caller
+ret                  ;; [3] Return to caller
 
 ;;
 ;; Global symbols to be used by external functions to set placeholders
