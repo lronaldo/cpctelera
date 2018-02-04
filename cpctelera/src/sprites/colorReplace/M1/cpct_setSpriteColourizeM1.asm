7048873f3262e8b29674e16c0f9fe84ad2d44e51
ronaldo <ronaldo@cheesetea.com>
2018-02-04 22:35:14 +0100

    Reviewed and documented setSpriteColourizeM1

--- cpctelera/src/sprites/colorReplace/M1/cpct_setSpriteColourizeM1.asm
+++ cpctelera/src/sprites/colorReplace/M1/cpct_setSpriteColourizeM1.asm
@@ -26,25 +26,40 @@
 ;; both colour to be replaced (*oldColour*) and replacement colour (*newColour*).
 ;;
 ;; C Definition:
-;;    void <cpct_setSpriteColourizeM1> (<u8> *oldColor*, <u8> *newColor*) __z88dk_callee;
+;;    void <cpct_setSpriteColourizeM1> (<u8> *oldColour*, <u8> *newColour*) __z88dk_callee;
 ;;
 ;; Input Parameters (2 bytes):
-;;  (1B L )  oldColor     - Colour to be replaced (Palette Index, 0-4)
-;;  (1B H )  newColor     - New colour (Palette Index, 0-4)
+;;  (1B L )  oldColour     - Colour to be replaced (Palette Index, 0-3)
+;;  (1B H )  newColour     - New colour (Palette Index, 0-3)
 ;;
 ;; Assembly call (Input parameters on registers):
 ;;    > call cpct_setSpriteColourizeM1_asm
 ;;
 ;; Parameter Restrictions:
-;;  * *oldColor* must be the palette index of the colour to be replaced (0 to 4).
-;;  * *newColor* must be the palette index of the new colour (0 to 4).
-;;
-;; Known limitations:
-;;     * This function works from ROM, but <cpct_spriteColourizeM0> must 
-;; be in RAM, as it gets modified by this function.
+;;  * *oldColour* (0-3) must be the palette index of the colour to be replaced.
+;;  * *newColour* (0-3) must be the palette index of the new colour.
 ;;
 ;; Details:
-;;    <TODO>
+;;    This function sets the colours that <cpct_spriteColourizeM1> will use 
+;; internally when called. It sets the colour to be searched (*oldColour*) and
+;; the replacement colour (*newColour*). Once set, a call to <cpct_spriteColourizeM1> 
+;; will transform each *oldColour* pixel into a *newColour* one. 
+;;  	The function should be called at least once before using 
+;; <cpct_spriteColourizeM1>. Otherwise, <cpct_spriteColourizeM1> would have
+;; no effect at all (it would replace colour 0 by colour 0).
+;; 	This function works by modifying <cpct_spriteColourizeM1>'s machine code, 
+;; changing values of *oldColour* and *newColour*. Therefore, the change is 
+;; permanent until a new change is performed. So you may call this function 
+;; once and then call <cpct_spriteColourizeM1> many times using the same
+;; colour configuration. You may see a code example by consulting 
+;; <cpct_spriteColourizeM1>'s documentation.
+;;
+;; Known limitations:
+;;    * This function works from ROM, but <cpct_spriteColourizeM1> must 
+;; be in RAM, as it gets modified by this function.
+;;    * This function does not check for parameters being valid. Values 
+;; outside (0-3) will produce undefined behaviour, probably corrupting
+;; colour values of any sprite modified with <cpct_spriteColourizeM1>.
 ;;
 ;; Destroyed Register values: 
 ;;    A, BC, HL
@@ -99,7 +114,7 @@ ld (cpct_spriteColourizeM1_px0_newval), a ;; [4]  Set replacement for Pixel-0 in
 
 ;; Convert oldColour to pixel format and
 ;; save it into cpct_setSpriteColourizeM1 machine code placeholders
-ld a, l                                   ;; [1]  A = H new colour index
+ld a, l                                   ;; [1]  A = L old colour index
 cpctm_lutget8 dc_mode1_ct, b, c           ;; [10] A = dc_mode0_ct[BC + A]. Get Pixel-3 searched colour from Look-Up-Table
 ld (cpct_spriteColourizeM1_px3_oldval), a ;; [4]  Set searched for Pixel-3 in cpct_setSpriteMaskedColourizeM1
 
