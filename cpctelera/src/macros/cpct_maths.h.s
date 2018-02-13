02fd1598ba4a90970772a66e2f3ce00209ceca35
ronaldo <ronaldo@cheesetea.com>
2018-02-14 00:40:37 +0100

    Added macros for 16-to-8 bit substraction

--- cpctelera/src/macros/cpct_maths.h.s
+++ cpctelera/src/macros/cpct_maths.h.s
@@ -192,3 +192,169 @@
 .macro add_bc_a
    add_REGPAIR_a  b, c
 .endm
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Macro: sub_REGPAIR_a 
+;;
+;;    Performs the operation REGPAIR = REGPAIR - A. REGPAIR is any given pair of 8-bit registers.
+;;
+;; ASM Definition:
+;;    .macro <sub_REGPAIR_a> RH, RL
+;;
+;; Parameters:
+;;    RH    - Register 1 of the REGPAIR. Holds higher-byte value
+;;    RL    - Register 2 of the REGPAIR. Holds lower-byte value
+;;  ?JMPLBL - Optional Jump label. A temporal one will be produced if none is given.
+;; 
+;; Input Registers: 
+;;    RH:RL - 16-value used as left-operand and final storage for the subtraction
+;;    A     - Second subtraction operand
+;;
+;; Return Value:
+;;    RH:RL - Holds the result of RH:RL - A
+;;
+;; Details:
+;;    This macro performs the subtraction of RH:RL - A and stores it directly on RH:RL.
+;; It uses only RH:RL and A to perform the operation.
+;;    With respect to the optional label ?JMPLBL, it is often better not to provide 
+;; this parameter. A temporal local symbol will be automatically generated for that label.
+;; Only provide it when you have a specific reason to do that.
+;;
+;; Modified Registers: 
+;;    A, RH, RL
+;;
+;; Required memory:
+;;    7 bytes
+;;
+;; Time Measures:
+;; (start code)
+;;  Case | microSecs(us) | CPU Cycles
+;; ------------------------------------
+;;  Any  |       7       |     28
+;; ------------------------------------
+;; (end code)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.macro sub_REGPAIR_a rh, rl, ?jmplbl
+   ;; First Perform A' = A - 1 - RL 
+   ;; (Inverse subtraction minus 1, used  to test for Carry, needed to know when to subtract 1 from RH)
+   dec    a          ;; [1] --A (In case A == RL, inverse subtraction should produce carry not to decrement RH)
+   sub   rl          ;; [1] A' = A - 1 - RL
+   jr     c, jmplbl  ;; [2/3] If A <= RL, Carry will be produced, and no decrement of RH is required, so jump over it
+     dec   rh        ;; [1] --RH (A > RL, so RH must be decremented)
+jmplbl:   
+   ;; Now invert A to get the subtraction we wanted 
+   ;; { RL' = -A' - 1 = -(A - 1 - RL) - 1 = RL - A }
+   cpl            ;; [1] A'' = RL - A (Original subtraction we wanted, calculated trough one's complement of A')
+   ld    rl, a    ;; [1] Save into RL (RL' = RL - A)
+.endm
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Macro: sub_de_a 
+;;
+;;    Performs the operation DE = DE - A. DE is any given pair of 8-bit registers.
+;;
+;; ASM Definition:
+;;    .macro <sub_de_a>
+;; 
+;; Input Registers: 
+;;    DE - 16-value used as left-operand and final storage for the subtraction
+;;    A  - Second subtraction operand
+;;
+;; Return Value:
+;;    DE - Holds the result of DE - A
+;;
+;; Details:
+;;    This macro performs the subtraction of DE - A and stores it directly on DE.
+;; It uses only DE and A to perform the operation.
+;;
+;; Modified Registers: 
+;;    A, DE
+;;
+;; Required memory:
+;;    7 bytes
+;;
+;; Time Measures:
+;; (start code)
+;;  Case | microSecs(us) | CPU Cycles
+;; ------------------------------------
+;;  Any  |       7       |     28
+;; ------------------------------------
+;; (end code)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.macro sub_de_a
+   sub_REGPAIR_a  d, e
+.endm
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Macro: sub_hl_a 
+;;
+;;    Performs the operation HL = HL - A. HL is any given pair of 8-bit registers.
+;;
+;; ASM Definition:
+;;    .macro <sub_hl_a>
+;; 
+;; Input Registers: 
+;;    HL - 16-value used as left-operand and final storage for the subtraction
+;;    A  - Second subtraction operand
+;;
+;; Return Value:
+;;    HL - Holds the result of HL - A
+;;
+;; Details:
+;;    This macro performs the subtraction of HL - A and stores it directly on HL.
+;; It uses only HL and A to perform the operation.
+;;
+;; Modified Registers: 
+;;    A, HL
+;;
+;; Required memory:
+;;    7 bytes
+;;
+;; Time Measures:
+;; (start code)
+;;  Case | microSecs(us) | CPU Cycles
+;; ------------------------------------
+;;  Any  |       7       |     28
+;; ------------------------------------
+;; (end code)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.macro sub_hl_a
+   sub_REGPAIR_a  h, l
+.endm
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Macro: sub_bc_a 
+;;
+;;    Performs the operation BC = BC - A. BC is any given pair of 8-bit registers.
+;;
+;; ASM Definition:
+;;    .macro <sub_bc_a>
+;; 
+;; Input Registers: 
+;;    BC - 16-value used as left-operand and final storage for the subtraction
+;;    A  - Second subtraction operand
+;;
+;; Return Value:
+;;    BC - Holds the result of BC - A
+;;
+;; Details:
+;;    This macro performs the subtraction of BC - A and stores it directly on BC.
+;; It uses only BC and A to perform the operation.
+;;
+;; Modified Registers: 
+;;    A, BC
+;;
+;; Required memory:
+;;    7 bytes
+;;
+;; Time Measures:
+;; (start code)
+;;  Case | microSecs(us) | CPU Cycles
+;; ------------------------------------
+;;  Any  |       7       |     28
+;; ------------------------------------
+;; (end code)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+.macro sub_bc_a
+   sub_REGPAIR_a  b, c
+.endm
