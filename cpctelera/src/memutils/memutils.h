e67ae9b29418214a6ed473b54bceec68efda2d7f
Arnaud <arnaud_bouche@hotmail.com>
2018-03-06 19:11:13 +0100

    Add cpct_memset_fx_i functions interrupt friendly

--- cpctelera/src/memutils/memutils.h
+++ cpctelera/src/memutils/memutils.h
@@ -31,10 +31,12 @@
 #include "banks.h"
 
 // Standard memory management functions
-extern void cpct_memset    (void *array, u8  value, u16 size) __z88dk_callee;
-extern void cpct_memset_f8 (void *array, u16 value, u16 size) __z88dk_callee;
-extern void cpct_memset_f64(void *array, u16 value, u16 size) __z88dk_callee;
-extern void cpct_memcpy    (void *to, const void *from, u16 size) __z88dk_callee;
+extern void cpct_memset      (void *array, u8  value, u16 size) __z88dk_callee;
+extern void cpct_memset_f8   (void *array, u16 value, u16 size) __z88dk_callee;
+extern void cpct_memset_f64  (void *array, u16 value, u16 size) __z88dk_callee;
+extern void cpct_memset_f64_i(void* array, u16 value, u16 size) __z88dk_callee;
+extern void cpct_memset_f8_i (void* array, u16 value, u16 size) __z88dk_callee;
+extern void cpct_memcpy      (void *to, const void *from, u16 size) __z88dk_callee;
 
 // Stack manipulation
 extern void cpct_setStackLocation(void *memory) __z88dk_fastcall;
