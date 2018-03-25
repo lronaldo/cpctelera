b2f308287429a8ead71b3cc41e0094d7c0f7696c
ronaldo <ronaldo@cheesetea.com>
2018-03-25 16:18:58 +0200

    Updated new project templates     * Added help on zgtiles parameter     * Moved makefiles' help to the end to clarify files     * Improved main.c basic example     * Added DSK folder to new projects for DSK file inclusion

--- cpctelera/tools/scripts/templates/main.c
+++ cpctelera/tools/scripts/templates/main.c
@@ -1,6 +1,6 @@
 //-----------------------------LICENSE NOTICE------------------------------------
 //  This file is part of CPCtelera: An Amstrad CPC Game Engine
-//  Copyright (C) 2015 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+//  Copyright (C) 2018 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
 //
 //  This program is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU Lesser General Public License as published by
@@ -20,9 +20,6 @@
 
 void main(void) {
    u8* pvmem;  // Pointer to video memory
-
-   // Clear Screen
-   cpct_memset(CPCT_VMEM_START, 0, 0x4000);
 
    // Draw String on the middle of the screen
    pvmem = cpct_getScreenPtr(CPCT_VMEM_START, 20, 96);
