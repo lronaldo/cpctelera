8f70782cab2fc3b4b786ef01c29daee9e18edff9
ronaldo <ronaldo@cheesetea.com>
2019-08-20 14:12:32 +0200

    UTILS: Added CONTAINED_IN    * Easy way to test for a value to be in a Set or not

--- cpctelera/cfg/modules/utils.mk
+++ cpctelera/cfg/modules/utils.mk
@@ -28,17 +28,19 @@
 ##   * ADD2SET
 ##   * ADD_N_ITEMS
 ##   * CHECKVARIABLEISSET
+##   * CONTAINED_IN
 ##   * CONVERTVALUE
 ##   * CONVERT_FW2HW_PALETTE
 ##   * DEC2HEX
+##   * EQUALS
 ##   * ENSURE_ADDRESS_VALID
 ##   * ENSURE_VALID_C_ID
 ##   * ENSURE_SINGLE_VALUE
 ##   * ENSUREVALID
 ##   * ENSUREFILEEXISTS
-##	 * FILEEXISTS
-##	 * FOLDEREXISTS
-##	 * FOLDERISWRITABLE
+##	  * FILEEXISTS
+##	  * FOLDEREXISTS
+##	  * FOLDERISWRITABLE
 ##   * GET_IMG_SIZE
 ##   * GREATER_THAN
 ##   * HEX2DEC
@@ -353,6 +355,18 @@ endef
 #
 define GREATER_THAN
 $(shell test "$(strip $(1))" -gt "$(strip $(2))" && echo true)
+endef
+
+#################
+# CONTAINED_IN: Checks if a value is contained in a set or not
+# It returns de value if it is contained or empty (false) if not.
+# It is thought to be used in conjunction with makefile if functions.
+#
+# $(1): Value to be checked in the set
+# $(2): Set of values separated by spaces
+#
+define CONTAINED_IN
+$(filter $(strip $(1)),$(strip $(2)))
 endef
 
 #################
