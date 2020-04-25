3b96ee913542ed90832360ead19bafe2a21d85e3
ronaldo <ronaldo@cheesetea.com>
2020-04-25 19:58:19 +0200

    CPCtelera new project CFG template:
     - build_config now includes android.mk to enable the exporter

--- cpctelera/tools/scripts/templates/newprj/cfg/build_config.tmpl.mk
+++ cpctelera/tools/scripts/templates/newprj/cfg/build_config.tmpl.mk
@@ -1,6 +1,6 @@
 ##-----------------------------LICENSE NOTICE------------------------------------
 ##  This file is part of CPCtelera: An Amstrad CPC Game Engine 
-##  Copyright (C) 2015 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
+##  Copyright (C) 2020 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
 ##
 ##  This program is free software: you can redistribute it and/or modify
 ##  it under the terms of the GNU Lesser General Public License as published by
