34cfd0edd1c0ef27226061ea575d2b239e55cad9
nestornillo <52611892+nestornillo@users.noreply.github.com>
2021-09-14 17:17:50 +0200

    cpct_setInterruptHandler minor documentation fix
    -Added cpct_setInterruptHandler_allRegisters reference
    -Added missing 'IY'

--- cpctelera/src/firmware/cpct_setInterruptHandler.s
+++ cpctelera/src/firmware/cpct_setInterruptHandler.s
@@ -1,5 +1,5 @@
 ;;-----------------------------LICENSE NOTICE------------------------------------
-;;  This file is part of CPCtelera: An Amstrad CPC Game Engine 
+;;  This file is part of CPCtelera: An Amstrad CPC Game Engine
 ;;  Copyright (C) 2015 ronaldo / Fremos / Cheesetea / ByteRealms (@FranGallegoBR)
 ;;
 ;;  This program is free software: you can redistribute it and/or modify
@@ -31,58 +31,59 @@
 ;;
 ;; Input Parameters (2 Bytes):
 ;;   (2B HL) intHandler - A pointer to the function that will handle interrupts from now on
-;; 
+;;
 ;; Assembly call:
 ;;    > call cpct_setInterruptHandler_asm
 ;;
 ;; Parameter Restrictions:
-;;  * *intHandler* (HL) can theoretically be any 16-bit value. However, it must point to 
-;; the start of a function that will be called at each interrupt. If it points to somewhere else, 
-;; undefined behaviour will happen. Most probably, the program will crash as random code will 
+;;  * *intHandler* (HL) can theoretically be any 16-bit value. However, it must point to
+;; the start of a function that will be called at each interrupt. If it points to somewhere else,
+;; undefined behaviour will happen. Most probably, the program will crash as random code will
 ;; be executed (that placed at the provided address). User defined function (*intHandler*)
 ;; must not return any value nor accept any parameters.
 ;;
 ;; Details:
 ;;    It modifies the interrupt vector to establish a new interrupt handler that calls
-;; user provided function. The user must provide a pointer to the function (*intHandler*) 
+;; user provided function. The user must provide a pointer to the function (*intHandler*)
 ;; that will handle interrupts. This function does not call *intHandler* or check it
 ;; in any way. It just sets it for being called at the next interrupts. Provided *intHandler*
-;; function must not return any value nor accept any parameter. 
+;; function must not return any value nor accept any parameter.
 ;;
 ;;    This function creates wrapper code to safely call user provided *intHandler*. This
-;; code saves registers on the stack (AF, BC, DE, HL and IX) and restores them after 
+;; code saves registers on the stack (AF, BC, DE, HL, IX and IY) and restores them after
 ;; user code from *intHandler* finishes. Therefore, user does not have to worry about
-;; saving registers. However, *be very careful if you modify the alternate set of 
-;; registers* (AF', BC', DE', HL') as they will not be saved by default. User is 
+;; saving registers. However, *be very careful if you modify the alternate set of
+;; registers* (AF', BC', DE', HL') as they will not be saved by default. User is
 ;; responsible for saving and restoring the values on the alternate register set
-;; whenever they are modified. Otherwise, behaviour is undefined when returning from 
-;; the function.
+;; whenever they are modified. Otherwise, behaviour is undefined when returning from
+;; the function. You can use <cpct_setInterruptHandler_allRegisters> if you want to
+;; save all registers (standard and alternate).
 ;;
-;; Destroyed Register values: 
+;; Destroyed Register values:
 ;;    A, HL
 ;;
 ;; Required memory:
 ;;    40 bytes (17 bytes function + 23 bytes for safe interrupt wrapper code)
 ;;
 ;; Time Measures:
-;;  * This first measure is the time required for cpct_setInterruptHandler to 
+;;  * This first measure is the time required for cpct_setInterruptHandler to
 ;; execute: that is, the time for setting up the hook for the system to call
 ;; user defined code.
 ;; (start code)
 ;; Case | microSecs (us) | CPU Cycles
 ;; -------------------------------------
-;; Any  |      23        |    92 
+;; Any  |      24        |    96
 ;; -------------------------------------
 ;; (end code)
-;;  * This second measure is the time overhead required for safely calling 
+;;  * This second measure is the time overhead required for safely calling
 ;; user defined function. That is, time required by the wrapper code to save
 ;; registers, call user's *intHandler*, restoring the registers and returning.
-;; This overhead is to be assumed each time interrupt handler is called, so 
+;; This overhead is to be assumed each time interrupt handler is called, so
 ;; up to 6 times per frame, 300 times per second.
 ;; (start code)
 ;; Case      | microSecs (us) | CPU Cycles
 ;; ------------------------------------------
-;; Overhead  |      58        |    184
+;; Overhead  |      57        |    228
 ;; ------------------------------------------
 ;; (end code)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -90,7 +91,7 @@
 _cpct_setInterruptHandler::
 cpct_setInterruptHandler_asm::
    ld (cpct_safeInterruptHandlerCall+1), hl;; [5] Place the pointer to the user code just after the call instruction
-   
+
    ;; Modify interrupt vector to call save interrupt handler hook (code below)
    ld  a, #0xC3                            ;; [2] 0xC3 = JP instruction, that may have been removed by other functions
    di                                      ;; [1] Disable interrupts
@@ -103,8 +104,8 @@ cpct_setInterruptHandler_asm::
 
 ;;
 ;; Interrupt Handler Safe Wrapper Code. This is the code that
-;; will be called at the start of the interrupt, and this code 
-;; will call user defined function, after saving registers. It 
+;; will be called at the start of the interrupt, and this code
+;; will call user defined function, after saving registers. It
 ;; also returns using reti for user comfortability.
 ;;  Overhead: 48 microsecs
 ;;
@@ -115,16 +116,16 @@ cpct_safeInterruptHandlerHook::
    push de     ;; [4]
    push hl     ;; [4]
    push ix     ;; [5]
-   push iy     ;; [5]
+   push iy     ;; [4]
 
-cpct_safeInterruptHandlerCall:   
+cpct_safeInterruptHandlerCall:
    call #0000  ;; [5] Call Interrupt Handler
 
-   pop  iy     ;; [5] Restore all standard registers
-   pop  ix     ;; [5]
+   pop  iy     ;; [4] Restore all standard registers
+   pop  ix     ;; [4]
    pop  hl     ;; [3]
    pop  de     ;; [3]
    pop  bc     ;; [3]
    pop  af     ;; [3]
    ei          ;; [1] Reenable interrupts
-   reti        ;; [3] Return to main program
+   reti        ;; [4] Return to main program
