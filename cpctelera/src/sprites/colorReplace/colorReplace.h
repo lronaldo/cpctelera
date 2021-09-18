b976147233f1a06adff5973c3a5fff9ad9917820
Arnaud <arnaud_bouche@hotmail.com>
2021-09-18 08:12:11 +0200

    Update API with new draw sprite color for M1 functions

--- cpctelera/src/sprites/colorReplace/colorReplace.h
+++ cpctelera/src/sprites/colorReplace/colorReplace.h
@@ -42,8 +42,8 @@ extern void cpct_drawSpriteMaskedColorizeM0(u8* sprite, u8* destMem, u8 width, u
 extern void cpct_drawSpriteMaskedAlignedColorizeM0(u8* sprite, u8* destMem, u8 width, u8 height, const u8* maskTable, u16 rplcPat) __z88dk_callee;
 
 // Functions to draw a sprite M1 with a color replaced 
-extern void cpct_drawSpriteColorizeM1(u8* sprite, u8* destMem, u8 width, u8 height, u8 oldColor, u8 newColor) __z88dk_callee;
-extern void cpct_drawSpriteMaskedColorizeM1(u8* sprite, u8* destMem, u8 width, u8 height, u8 oldColor, u8 newColor) __z88dk_callee;
-extern void cpct_drawSpriteMaskedAlignedColorizeM1(u8* sprite, u8* destMem, u8 width, u8 height, u8 oldColor, u8 newColor, const u8* maskTable) __z88dk_callee;
+extern void cpct_drawSpriteColorizeM1(u8* sprite, u8* destMem, u8 width, u8 height, u16 rplcPat) __z88dk_callee;
+extern void cpct_drawSpriteMaskedColorizeM1(u8* sprite, u8* destMem, u8 width, u8 height, u16 rplcPat) __z88dk_callee;
+extern void cpct_drawSpriteMaskedAlignedColorizeM1(u8* sprite, u8* destMem, u8 width, u8 height, const u8* maskTable, u16 rplcPat) __z88dk_callee;
 
 #endif
