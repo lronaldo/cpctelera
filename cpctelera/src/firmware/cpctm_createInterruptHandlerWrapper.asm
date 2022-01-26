3fceea1105207641c485ed74a5867eea900f3c31
nestornillo <52611892+nestornillo@users.noreply.github.com>
2022-01-26 19:31:41 +0100

    Moved code into cpctm_createInterruptHandler

--- cpctelera/src/firmware/cpctm_createInterruptHandlerWrapper.asm
+++ cpctelera/src/firmware/cpctm_createInterruptHandlerWrapper.asm
@@ -154,6 +154,7 @@
   pop iy       ;; [4]
 .endm
 
+.mdelete cpctm_createInterruptHandlerWrapper_asm
 .macro cpctm_createInterruptHandlerWrapper_asm WrapperName, intHandAddress, R1, R2, R3, R4, R5, R6, R7, R8, R9, R10, R11
   .equ cpct_altAFdetected, 0
   .equ cpct_altBCDEHLdetected, 0
